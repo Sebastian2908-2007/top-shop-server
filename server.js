05a68c68b2d06b8d6e59d2f465ac4cf55c150f1e
Sebabin <sebastiancrespin279@gmail.com>
2023-02-27 23:50:27 -0700

    what I did prior broke everything so yeah I put it back

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const app = express();
 /**make apolloserver function */
 const startServer = async () => {
     const server = new ApolloServer({
+        cors: corsOptions,
         typeDefs,
         resolvers,
         /*csrfPrevention: true,
@@ -32,7 +33,7 @@ const startServer = async () => {
     await server.start();
 
     /**apply express as middleware */
-    server.applyMiddleware({app});
+    server.applyMiddleware({ app, cors: false  });
 
     /*log grapql url */
    console.log(`Use graphql at http://localhost:${PORT}${server.graphqlPath}`);
